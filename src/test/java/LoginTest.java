5b76fdc441c63fdc75c1efcf7985c45e474a8502
vishaka <tmvishakha@gmail.com>
2021-02-04 18:48:06 +0530

    Commited pom and src

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+public static WebDriver driver;
+@BeforeSuite
+	public void setUp(){
+	
+	System.setProperty("webdriver.chrome.driver",".\\Browser\\chromedriver.exe");
+	driver=new ChromeDriver();
+	}
+
+	@Test
+	public void doLogin(){
+		driver.get("http://way2automation.com/");
+	}
+	
+	 @AfterSuite
+	  public void closeBrowser(){
+			driver.close();
+		}
+}
